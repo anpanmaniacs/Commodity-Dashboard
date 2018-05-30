a0f55783a71fda6a50f24656fbcc37bd7c086ded
HAILGAMBO <gauravk.scb@gmail.com>
2018-05-30 02:21:53 -0700

    !

--- static/js/index_IE.js
+++ static/js/index_IE.js
@@ -1,4 +1,4 @@
-Plotly.d3.csv('../../data/exp.csv', function(err, rows){
+Plotly.d3.csv('../data/exp.csv', function(err, rows){
     function unpack(rows, key) {
         return rows.map(function(row) { return row[key]; });
     }
